c3f34cebea6a2d806a965c6293829cefe5a1c330
ZEKERİYA İSHAK DOĞANAY <zishakdoganay@ogr.trakya.edu.tr>
2023-08-27 23:34:28 +0300

    database connection bug fixed, all services working

--- DataAccess/Concrete/EntityFramework/ApolloContext.cs
+++ DataAccess/Concrete/EntityFramework/ApolloContext.cs
@@ -13,7 +13,7 @@ namespace DataAccess.Concrete.EntityFramework
         //Fixed My database Connection because my db is not connect with my backend codes.
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server = (localdb)\mssqllocaldb;Database=Apollo;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-SDKFJ6F;Initial Catalog=Apollo;Integrated Security=True;Trusted_Connection=true;TrustServerCertificate=True");
         }
 
         public DbSet<User> USERS { get; set; }
